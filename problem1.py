37bb9e17d788fc390e1c9e2026af4daa04a1b109
rishiannoy <rishirajpurohit013@gmail.com>
2019-06-12 20:08:48 -0700

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -2,4 +2,4 @@ import datetime
 name=input("Enter Your Name :")
 age=int(input("Enter your age :"))
 now = datetime.datetime.now()
-print(name," Will turn 95 in ",(95-age)+now.year)
+print(name," you will turn 95 at",(95-age)+now.year)
